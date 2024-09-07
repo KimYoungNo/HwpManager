b272b75a2f23aff90f6cd5f64457c12b86cc366b
KimYoungNo <132270647+KimYoungNo@users.noreply.github.com>
2024-09-07 18:53:54 +0900

    Update HwpManager.py

--- HwpManager/HwpManager.py
+++ HwpManager/HwpManager.py
@@ -1,1 +1,7 @@
+import win32com.client as win32
+from ._HwpRegistery import HwpSecurityModule
 
+def _new_hwp():
+  hwp = win32.gencache.EnsureDispatch('HWPFrame.HwpObject')
+  hwp.RegisterModule("FilePathCheckDLL", str(HwpSecurityModule))
+  return hwp
