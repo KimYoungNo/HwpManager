4ddbc575b1c34f9991a9a4f494b27beca5bdcf4b
KimYoungNo <132270647+KimYoungNo@users.noreply.github.com>
2024-09-07 18:27:53 +0900

    Create _HwpRegistery.py

---
+++ HwpManager/_HwpRegistery.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+from enum import Enum
+
+class HwpSecurityReg(str, Enum):
+    base_path = os.path.join(os.getcwd(), "Windows_HwpSecurityModule_Register")
+    register_path = os.path.join(base_path, "Register.bat")
+    unregister_path = os.path.join(base_path, "Unregister.bat")
+
+for pth in HwpSecurityReg:
+    if not os.path.exists(pth.value):
+        raise OSError(f"Unknown Path: {pth.value}")
+
+
+def _RunBatch(batch_path, **kwargs):
+    return subprocess.run([batch_path], **kwargs)
+    
+def HwpSecurityModuleRegister():
+    return _RunBatch(HwpSecurityReg.register_path.value, timeout=5)
+    
+def HwpSecurityModuleUnregister():
+    return _RunBatch(HwpSecurityReg.unregister_path.value, timeout=5)
+
+
+class HwpSecurityModule:
+    def __enter__(self):
+        HwpSecurityModuleRegister()
+        
+    def __exit__(self, exc_val, exc_type, exc_trace):
+        HwpSecurityModuleUnregister()
