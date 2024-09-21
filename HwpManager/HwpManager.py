ae0089d20356cb8927f7cc138b971dcfa1dca556
KimYoungNo <132270647+KimYoungNo@users.noreply.github.com>
2024-09-21 16:42:49 +0900

    Update HwpManager.py

--- HwpManager/HwpManager.py
+++ HwpManager/HwpManager.py
@@ -1,11 +1,10 @@
 import time
 import pythoncom as pycom
-import win32con as con32
 import win32gui as gui32
-import win32com.client as win32
+import win32com.client as com32
 from collections import deque
 from dataclasses import dataclass
-from ._HwpRegistery import HwpSecurityModule
+from ._HwpRegistery import FilePathCheckDLL
 
 def _enumerate_hwps():
     context = pycom.CreateBindCtx(0)
@@ -14,122 +13,159 @@ def _enumerate_hwps():
     hwp_monikers = tuple(moniker for moniker in running_coms.EnumRunning()
         if "!HwpObject"==moniker.GetDisplayName(context, moniker).split('.', 1)[0])
     
-    return tuple(com32.Dispatch(running_coms.GetObject(moniker).QueryInterface(IID_IDispatch))
+    return tuple(com32.Dispatch(running_coms.GetObject(moniker).QueryInterface(pycom.IID_IDispatch))
         for moniker in hwp_monikers)
 
 def _grab_hwp():
     hwnd = gui32.GetForegroundWindow()
-    hwp = None
 
     if hwnd != 0:
         window_name = gui32.GetWindowText(hwnd)
         
         for hwp in _enumerate_hwps():
             try:
-                filepath, filename = hwp.XHwpDocuments.Active_XHwpDocument.FullName.rsplit('\\')
-            
+                filepath, filename = hwp.XHwpDocuments.Active_XHwpDocument.FullName.rsplit('\\', 1)
+                
                 if f"{filename} [{filepath}\\] - 한글" == window_name:
-                    hwp = _register_hwp(hwp)
+                    return hwp
             except:
                 continue
-    return hwp
+    return None
     
 def _new_hwp():
-    hwp = win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
-    return _register_hwp(hwp)
+    hwp = com32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+    return hwp
 
 def _register_hwp(hwp):
-    hwp.RegisterModule("FilePathCheckDLL", str(HwpSecurityModule))
+    hwp.RegisterModule("FilePathCheckDLL", str(FilePathCheckDLL))
     return hwp
 
 class _HwpWrapper:
     def __init__(self, hwp):
-        self._hwp = hwp
+        self._hwp = _register_hwp(hwp)
+        
+    def __bool__(self):
+        try:
+            self._hwp.XHwpDocuments.Active_XHwpDocument.DocumentID
+        except:
+            return False
+        else:
+            return True
 
     def __getattr__(self, name):
         return getattr(self._hwp, name)
 
     def __del__(self):
-        self._hwp.Quit()
+        try:
+            self._hwp.Quit()
+        except:
+            pass
 
     def __str__(self):
-        return self._hwp.XHwpDocuments.Active_XHwpDocument.FullName
-
-    def __bool__(self):
-        try:
-            self._hwp.CheckXObject(True)
-        except:
-            return False
+        if self:
+            return self._hwp.XHwpDocuments.Active_XHwpDocument.FullName
         else:
-            return True
+            return ''
+            
+    def Open(self, filepath):
+        self._hwp.Open(filepath,
+            arg="versionwarning:False;forceopen:True;suspendpassword:True")
+            
+    def Visible(self, option=True):
+        if self:
+            self._hwp.XHwpWindows.Item(0).Visible = option
+            
+    def Release(self):
+        self.Visible(True)
+        self._hwp = None
+            
+class _HwpQueue(deque):
+    def __del__(self):
+        for hwp in self:
+            del hwp
     
+    def append(self, hwp):
+        super().append(_HwpWrapper(hwp))
 
 class HwpManager:
-    _hwps = deque()
+    _inst = None
+    _hwps = _HwpQueue()
+    
+    def __new__(cls, *_):
+        if cls._inst is None:
+            cls._inst = object.__new__(cls)
+        return cls._inst
         
-    def __init__(self, hwp_id=None):
+    def __init__(self, hwp_id=-1):
         self._hwp_id = hwp_id
-
-    def __del__(self):
-        self.Release()
 
     @classmethod
     def __len__(cls):
         return len(cls._hwps)
+        
+    @classmethod
+    def __iter__(cls):
+        return iter(cls._hwps)
 
     @classmethod
     def __getitem__(cls, index):
         return cls._hwps[index]
-
-    @classmethod
-    def _RenewSecurityModule(cls):
-        num = len(cls._hwps)
         
-        if num < 1:
-            HwpSecurityModule.Unregister()
-        elif num == 1:
-            HwpSecurityModule.Register()
-
-    def _AfterAppend(self):
-        self._hwp_id = -1
-        self._RenewSecurityModule()
-
-    @classmethod
-    def _DequeInvalidHwp(cls):
-        invalids = tuple(hwp for hwp in cls._hwps if not hwp)
-
-        for invalid_hwp in invalids:
-            cls._hwps.remove(invalid_hwp)
+    def __bool__(self):
+        return bool(self.__class__._hwps[self._hwp_id])
+        
+    def __getattr__(self, name):
+        return getattr(self.__class__._hwps[self._hwp_id], name)
         
     def New(self):
-        self.__class__._hwps.append(_HwpWrapper(_new_hwp()))
-        self._AfterAppend()
+        self.__class__._hwps.append(_new_hwp())
+        self._hwp_id = -1
 
     def Grab(self):
-        self.__class__._hwps.append(_HwpWrapper(_grab_hwp()))
-        self._AfterAppend()
-
-    def Select(self, nth):
         hwps = self.__class__._hwps
-        hwp_id = nth % len(hwps)
+        hwp = None
         
-        if not hwps[hwp_id].occupied:
-            hwps[hwp_id].occupied = True
-            hwps[self._hwp_id].occupied = False
-            self._hwp_id = hwp_id
+        while True:
+            hwp = _grab_hwp()
+            
+            if hwp is not None:
+                break
+            
+        if hwp in hwps:
+            self._hwp_id = hwps.index(hwp)
         else:
-            ValueError("Pre-ocuupied Instance Selected")
+            hwps.append(hwp)
+            self._hwp_id = -1
+
+    def Select(self, index):
+        self._hwp_id = index % len(self)
 
     def Release(self):
-        self._hwp_id = None
-        self._RenewSecurityModule()
+        hwps = self.__class__._hwps
+        
+        hwps[self._hwp_id].Release()
+        del hwps[self._hwp_id]
+        self._hwp_id = -1
+        
+    def Refresh(self):
+        hwps = self.__class__._hwps
+        invalids = tuple(hwp for hwp in hwps if not hwp)
+
+        for invalid_hwp in invalids:
+            hwps.remove(invalid_hwp)
+        
+        if invalids:
+            self._hwp_id = -1
     
-    @classmethod
-    def KillAll(cls):
-        for hwp in cls._hwps:
-            try:
-                del hwp
-            except:
-                pass
-        cls._hwps.clear()
+    def KillAll(self):
+        hwps = self.__class__._hwps
+        
+        for hwp in hwps:
+            del hwp
+                
+        hwps.clear()
+        self._hwp_id = -1
     
+    @property
+    def CurrentID(self):
+        return self._hwp_id
