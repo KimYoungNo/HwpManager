789fd2a8497f83882eb9f676e65e0a060d63cb19
KimYoungNo <132270647+KimYoungNo@users.noreply.github.com>
2024-09-22 19:00:30 +0900

    Update HwpUtils.py

--- HwpManager/HwpUtils.py
+++ HwpManager/HwpUtils.py
@@ -1,1 +1,11 @@
-
+def PrintPDF(hwp, output_path, printer_name, print_range=None):
+    if print_range is None:
+        print_range = f"1-{hwp.PageCount}"
+    
+    with hwp.HParameterSet("HPrint", "PrintToPDF") as HPrint:
+        HPrint.filename = output_path
+        HPrint.PrinterName = printer_name	
+        HPrint.PrintMethod = hwp.PrintType("Nomal")
+        HPrint.Device = hwp.PrintDevice("PDF")
+        HPrint.Range = hwp.PrintRange("Custom")
+        HPrint.RangeCustom = print_range
